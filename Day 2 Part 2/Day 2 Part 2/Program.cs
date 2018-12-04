8d21d07baafb6a1845ed077f13ccf4459105cbd2
ydderfBackwards <34340157+ydderfBackwards@users.noreply.github.com>
2018-12-04 20:12:35 +0100

    Add files via upload

---
+++ Day 2 Part 2/Day 2 Part 2/Program.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_2_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, numberFault;
+            char[] charArray;
+            char[] charArray2;
+            char[] charArrayFound = new char[30]; 
+            
+            //Read each line
+            foreach (string line in File.ReadLines(@"D:\Prive\Projecten\C#\AdventOfCode2018\Day 2 Part 2\input.txt", Encoding.UTF8))
+            {
+                //String to array of chars
+                charArray = line.ToCharArray();
+
+                //Read each line again (matches don't have to be directly below each other
+                foreach (string line2 in File.ReadLines(@"D:\Prive\Projecten\C#\AdventOfCode2018\Day 2 Part 2\input.txt", Encoding.UTF8))
+                {
+                    //String to array of chars
+                    charArray2 = line2.ToCharArray();
+
+                    //Reset to zero at nex compare
+                    numberFault = 0;
+
+                    //For each char in array
+                    for (i = 0; i < charArray.Length; i++)
+                    {
+                        //compare char
+                        if (charArray[i] != charArray2[i])
+                        {
+                            //Not the same
+                            charArrayFound[i] = ' ';
+                            numberFault += 1;
+                        }
+                        else
+                        {
+                            //same --> remember char
+                            charArrayFound[i] = charArray[i];
+                        }
+                    }
+
+                    //check number of faults
+                    if (numberFault == 1)
+                    {
+                        string lineFound = line;
+                        goto gotoItemFound;
+                    }
+
+
+                }
+            }
+
+
+            gotoItemFound:
+
+            //print result. Note: it wil allway print result even if no found.
+            Console.WriteLine("Result is: ");
+            for (i = 0; i < 30; i++)
+            {
+                Console.Write("{0}", charArrayFound[i]);
+
+            }
+              Console.ReadKey();
+
+        }
+    }
+}
